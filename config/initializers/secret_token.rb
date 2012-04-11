87552fa9e18780c21026c7a2837fd03536bae39b
Phil Monroe <phil@philmonroe.com>
2012-04-11 12:34:46 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoreProto::Application.config.secret_token = '705e7cabb2319c703b4ca07c95dfe59a37db19b33accf394e2ce13d6718d2d83f9af9dea56f1c605067a9634c60e3e6a7061d4a630a01bf471edaab93e6364a6'

00e9ca9d84d0b471e977cb7fe4e2195645391053
Tejas Ravindra Mandke <tejas@identified.com>
2012-04-11 20:34:59 -0700

    change Event by scope to except class or object

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,12 @@ class Event < ActiveRecord::Base
   belongs_to :user, :polymorphic => true
   belongs_to :available_event
 
-  scope :by, lambda { |type| where(:user_type => type)}
+  scope :by, lambda { |who|
+    if who.is_a? Class
+      where(:user_type => who)
+    else
+      where(:user_type => who.class, :user_id => who.id)
+    end
+  }
   scope :in_pillar, lambda {|pillar| joins(:available_event).where("available_events.pillar_id = #{pillar.id}") }
 end
